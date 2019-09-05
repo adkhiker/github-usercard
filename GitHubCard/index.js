87e4495bf14f6ca70918c6d4ca8ff800d8e3715f
adkhiker <poirier.greg@gmail.com>
2019-09-05 18:54:57 -0400

    MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -18,26 +18,6 @@ axios
     console.log("API Error", error);
   });
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
-
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
 
 const followersArray = [
   "https://api.github.com/users/fskeen",
